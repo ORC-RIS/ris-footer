8b23b6d1490c79467118e0ee6765637b02d676ff
John Lynch <lynchj1313@gmail.com>
2017-06-16 10:26:26 -0400

    Updated footer to include login information from the user using coldfusion session variables.

--- js/ris_footer.js
+++ js/ris_footer.js
@@ -1,5 +1,3 @@
-
-
 if(typeof jq !== 'undefined'){
 	jq.addEventListener("load", initialize_ris_footer, false);
 }
@@ -9,10 +7,8 @@ else {
 	document.querySelector('head').appendChild(jq);
 	jq.onload = initialize_ris_footer;
 }
-
 function initialize_ris_footer(){
 	ris_footer_jQuery = jQuery.noConflict(true);
-
 	var d = new Date();
 	var defaultInfo = { 
 		"leftBtns" : [
@@ -22,6 +18,7 @@ function initialize_ris_footer(){
 		],
 		"rightBtns" : [
 		{ "text":"link4", "url":"#"},
+		{ "text":"link5", "url":"#"},
 		{ "text":"Logout", "url":"#"}
 		],
 		"copyright" : {
@@ -30,20 +27,20 @@ function initialize_ris_footer(){
 			"email":"risservicedesk&#64;ucf.edu"
 		}
 	};
-
 	ris_footer_jQuery(function() {
-
-		
 		function mergeInformation(){	//Combines the defaultInfo with customInfo
 			for (var attr in customInfo) {
-				footerInfo[attr] = ris_footer_jQuery.extend(footerInfo[attr], customInfo[attr]);
+				if (attr.indexOf("Btns") != -1) {
+					footerInfo[attr] = customInfo[attr];
+				}
+				else
+					footerInfo[attr] = ris_footer_jQuery.extend(footerInfo[attr], customInfo[attr]);
 			}
 		}
-
 		function addButtons(buttons){
 			var list = "";
 			for (b = 0; b < buttons.length; b++){
-				list += '<a href="'+buttons[b].url+'">'+ buttons[b].text + '</a>\n';
+				list += '<a href="'+buttons[b].url+'"><strong>'+ buttons[b].text + '</strong></a>\n';
 			}
 			return list;
 		}
@@ -52,33 +49,41 @@ function initialize_ris_footer(){
 		if (typeof customInfo !== 'undefined'){
 			mergeInformation();	
 		}
-
 		var buttons = '<div class="leftBtns">\n'+addButtons(footerInfo.leftBtns)+"</div>" + 
 					  '<div class="rightBtns">\n'+addButtons(footerInfo.rightBtns)+"</div>";
 		var userInfo;
+		if (footerInfo.loginInfo != null){
+			userInfo =  "<div class='loginInfo'>"+
+				"User: <span id='username'>"+footerInfo.loginInfo.fName+", "+footerInfo.loginInfo.lName+"</span> "+
+				"Role: <span id='role'>"+footerInfo.loginInfo.role+"</span> " +
+				"Logged in from: <span id='IP'>"+footerInfo.loginInfo.IPaddress+"</span>"+
+				"</div>";
+		}else{
+			userInfo ="";
+		}
 
-		var a = document.createElement('link');
-		a.setAttribute("href", "styles/styles.css");
-		a.setAttribute("rel", "stylesheet");
+
+		var a = document.createElement('link');	
+		a.setAttribute("href", "styles/styles.css");		// Creates link to CSS file, href attribute
+		a.setAttribute("rel", "stylesheet");				// may need to be changed accordingly!
 		a.setAttribute("type", "text/css");
 		document.querySelector('head').appendChild(a);
 
 		ris_footer_jQuery("#ris_footer").html(' <div class="footer_wrapper"> ' +
 			'<div class="footer_buttons">'+buttons+'</div>' +
-			'<div style="clear:both;"></div> '+
-			'<div class="loginInfo">USER LOGIN INFO HERE</div>'+
-			'<hr>' +
+			'<div style="clear:both;"></div> '+userInfo+'<hr>' +
 			'<div class="footerCopy">' +
+				'<img src="images/ldg_ucflogo.png" style="margin-bottom: 5px;"/>' +
 				'<div class="copy_comp">&copy; '+d.getFullYear()+' '+footerInfo.copyright.name+'</div>' +
 				'<div class="copy_dev"><strong>Developed by '+footerInfo.copyright.dev+'</strong></div>' +
 				'<div class="copy_email">'+
 					'<a href="mailto:'+footerInfo.copyright.email+'">'+footerInfo.copyright.email+'</a>'+
 				'</div>' +
 			'</div>' +
-			'</div>');
+			'</div>'
+		);
+
 	})
 
 }
-
-
 
