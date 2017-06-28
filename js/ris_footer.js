e97d9f8cad64f1cf9da714d30edfc93ad33d4d80
Andrew Brandt <ABrandt@RESEARCH.UCF.EDU>
2017-06-28 13:53:33 -0400

    Updated ris_footer.js and styles.css. add TheTab_KGrgb_72ppi.png

--- js/ris_footer.js
+++ js/ris_footer.js
@@ -7,15 +7,16 @@ else {
 	document.querySelector('head').appendChild(jq);
 	jq.onload = initialize_ris_footer;
 }
+
 function initialize_ris_footer(){
 	ris_footer_jQuery = jQuery.noConflict(true);
 	var d = new Date();
 	var defaultInfo = { 
-		"leftBtns" : [
+		/*"leftBtns" : [
 		{ "text":"link1", "url":"#"},
 		{ "text":"link2", "url":"#"},
 		{ "text":"link3", "url":"#"},
-		],
+		],*/
 		"rightBtns" : [
 		{ "text":"link4", "url":"#"},
 		{ "text":"link5", "url":"#"},
@@ -27,6 +28,7 @@ function initialize_ris_footer(){
 			"email":"risservicedesk&#64;ucf.edu"
 		}
 	};
+
 	ris_footer_jQuery(function() {
 		function mergeInformation(){	//Combines the defaultInfo with customInfo
 			for (var attr in customInfo) {
@@ -37,6 +39,19 @@ function initialize_ris_footer(){
 					footerInfo[attr] = ris_footer_jQuery.extend(footerInfo[attr], customInfo[attr]);
 			}
 		}
+
+	function addBorder(){//Adds a border to the footer only if there are buttons or login information to display.
+		var string = "<hr class='border'>";
+		var empty = "";
+		if(((footerInfo.rightBtns != null) && (footerInfo.rightBtns != "")) || ((footerInfo.loginInfo != null) && (footerInfo.loginInfo != "")) || 
+			((footerInfo.leftBtns != null) && (footerInfo.leftBtns != ""))){
+		 	return string;
+		 }
+		 else{
+		 	return empty;
+		 }
+	}
+		
 		function addButtons(buttons){
 			var list = "";
 			for (b = 0; b < buttons.length; b++){
@@ -49,13 +64,13 @@ function initialize_ris_footer(){
 		if (typeof customInfo !== 'undefined'){
 			mergeInformation();	
 		}
-		var buttons = '<div class="leftBtns">\n'+addButtons(footerInfo.leftBtns)+"</div>" + 
+		var buttons = //'<div class="leftBtns">\n'+addButtons(footerInfo.leftBtns)+"</div>" + 
 					  '<div class="rightBtns">\n'+addButtons(footerInfo.rightBtns)+"</div>";
 		var userInfo;
 		if (footerInfo.loginInfo != null){
 			userInfo =  "<div class='loginInfo'>"+
-				"User: <span id='username'>"+footerInfo.loginInfo.fName+", "+footerInfo.loginInfo.lName+"</span> "+
-				"Role: <span id='role'>"+footerInfo.loginInfo.role+"</span> " +
+				"User: <span id='username'>"+footerInfo.loginInfo.fName+", "+footerInfo.loginInfo.lName+"</span><br/> "+
+				"Role: <span id='role'>"+footerInfo.loginInfo.role+"</span><br/> " +
 				"Logged in from: <span id='IP'>"+footerInfo.loginInfo.IPaddress+"</span>"+
 				"</div>";
 		}else{
@@ -69,16 +84,20 @@ function initialize_ris_footer(){
 		a.setAttribute("type", "text/css");
 		document.querySelector('head').appendChild(a);
 
-		ris_footer_jQuery("#ris_footer").html(' <div class="footer_wrapper"> ' +
-			'<div class="footer_buttons">'+buttons+'</div>' +
-			'<div style="clear:both;"></div> '+userInfo+'<hr>' +
+		ris_footer_jQuery("#ris_footer").html(' <div class="footer_wrapper"> ' +//Adds footer contents to page.
+			'<div class="footer_buttons">'+buttons+userInfo+'</div>' +
+			'<div style="clear:both;"></div> ' + addBorder() + //Calls function to add border if buttons or login info available.
 			'<div class="footerCopy">' +
-				'<img src="images/ldg_ucflogo.png" style="margin-bottom: 5px;"/>' +
-				'<div class="copy_comp">&copy; '+d.getFullYear()+' '+footerInfo.copyright.name+'</div>' +
-				'<div class="copy_dev"><strong>Developed by '+footerInfo.copyright.dev+'</strong></div>' +
-				'<div class="copy_email">'+
-					'<a href="mailto:'+footerInfo.copyright.email+'">'+footerInfo.copyright.email+'</a>'+
-				'</div>' +
+				'<div class="logo">' +
+					'<img src="TheTab_KGrgb_72ppi.png" style="margin-bottom: 5px;"/>' +
+				'</div>' +	
+				'<div class="text">' +
+					'<div class="copy_comp">&copy; '+d.getFullYear()+' '+footerInfo.copyright.name+'</div>' +
+					'<div class="copy_dev">Developed by '+footerInfo.copyright.dev+'</div>' +
+					'<div class="copy_email">'+
+						'<a href="mailto:'+footerInfo.copyright.email+'">'+footerInfo.copyright.email+'</a>'+
+					'</div>' +
+				'</div>' +	
 			'</div>' +
 			'</div>'
 		);
